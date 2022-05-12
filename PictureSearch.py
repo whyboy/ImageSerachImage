02108263e109be548f020aa35e6c8e7e663d6d28
whyboy <1032131939@qq.com>
2022-05-13 00:11:18 +0800

    LKG-code refactor

--- PictureSearch.py
+++ PictureSearch.py
@@ -5,64 +5,73 @@ import numpy as np
 import os
 import math
 
-def getMatchNum(matches,ratio):
-    '''返回特征点匹配数量和匹配掩码'''
-    matchesMask=[[0,0] for i in range(len(matches))]
-    matchNum=0
-    for i,(m,n) in enumerate(matches):
-        if m.distance<ratio*n.distance: #将距离⽐率⼩于ratio的匹配点删选出来
-            matchesMask[i]=[1,0]
-            matchNum+=1
-    return (matchNum,matchesMask)
+class searcher:
+    def __init__(self, samplePath, queryPath):
+        self.samplePath = samplePath
+        self.queryPath = queryPath
+
+        #创建SIFT特征提取器
+        self.sift = cv2.SIFT_create()
+
+        #创建FLANN匹配对象
+        FLANN_INDEX_KDTREE=0
+        indexParams=dict(algorithm=FLANN_INDEX_KDTREE,trees=5)
+        searchParams=dict(checks=50)
+        self.flann=cv2.FlannBasedMatcher(indexParams,searchParams)
 
 
-path='C:/Users/whaiyan/Desktop/PictureSearchPicture/'
-queryPath=path+'Pictures/' #图库路径
-samplePath=path+'Sample/sample.PNG' #样本图⽚
-comparisonImageList=[] #记录⽐较结果
+    def start(self):
+        sampleImage=cv2.imread(self.samplePath,0)
+        kp1, des1 = self.sift.detectAndCompute(sampleImage, None) #提取样本图⽚的特征
+
+        comparisonImageList = []
+        for parent,dirnames,filenames in os.walk(self.queryPath):
+            for p in filenames:
+                p=queryPath+p
+                queryImage=cv2.imread(p,0)
+                kp2, des2 = self.sift.detectAndCompute(queryImage, None) #提取⽐对图⽚的特征
+                matches=self.flann.knnMatch(des1,des2,k=2) #匹配特征点，为了删选匹配点，指定k为2，这样对样本图的每个特征点，返回两个匹配
+                (matchNum,matchesMask)=self.__getMatchNum(matches,0.9) #通过⽐率条件，计算出匹配程度
+                matchRatio=matchNum*100/len(matches)
+                drawParams=dict(matchColor=(0,255,0),
+                        singlePointColor=(255,0,0),
+                        matchesMask=matchesMask,
+                        flags=0)
+                comparisonImage=cv2.drawMatchesKnn(sampleImage,kp1,queryImage,kp2,matches,None,**drawParams)
+                comparisonImageList.append((comparisonImage,matchRatio)) #记录下结果
+
+        comparisonImageList.sort(key=lambda x:x[1],reverse=True) #按照匹配度排序
+        return comparisonImageList
+
+    
+    def __getMatchNum(self, matches,ratio):
+        '''返回特征点匹配数量和匹配掩码'''
+        matchesMask=[[0,0] for i in range(len(matches))]
+        matchNum=0
+        for i,(m,n) in enumerate(matches):
+            if m.distance<ratio*n.distance: #将距离⽐率⼩于ratio的匹配点删选出来
+                matchesMask[i]=[1,0]
+                matchNum+=1
+        return (matchNum,matchesMask)
 
 
-#创建SIFT特征提取器
-sift = cv2.SIFT_create()
+if __name__ == "__main__":
+    path='C:/Users/whaiyan/Desktop/PictureSearchPicture/'
+    queryPath=path+'Pictures/' #图库路径
+    samplePath=path+'Sample/sample.PNG' #样本图⽚
 
-#创建FLANN匹配对象
-FLANN_INDEX_KDTREE=0
-indexParams=dict(algorithm=FLANN_INDEX_KDTREE,trees=5)
-searchParams=dict(checks=50)
-flann=cv2.FlannBasedMatcher(indexParams,searchParams)
+    obj = searcher(samplePath, queryPath)
+    comparisonImageList=obj.start()
 
-sampleImage=cv2.imread(samplePath,0)
-kp1, des1 = sift.detectAndCompute(sampleImage, None) #提取样本图⽚的特征
-
-for parent,dirnames,filenames in os.walk(queryPath):
-    for p in filenames:
-        p=queryPath+p
-        queryImage=cv2.imread(p,0)
-        kp2, des2 = sift.detectAndCompute(queryImage, None) #提取⽐对图⽚的特征
-        matches=flann.knnMatch(des1,des2,k=2) #匹配特征点，为了删选匹配点，指定k为2，这样对样本图的每个特征点，返回两个匹配
-        (matchNum,matchesMask)=getMatchNum(matches,0.9) #通过⽐率条件，计算出匹配程度
-        matchRatio=matchNum*100/len(matches)
-        drawParams=dict(matchColor=(0,255,0),
-                singlePointColor=(255,0,0),
-                matchesMask=matchesMask,
-                flags=0)
-        comparisonImage=cv2.drawMatchesKnn(sampleImage,kp1,queryImage,kp2,matches,None,**drawParams)
-        comparisonImageList.append((comparisonImage,matchRatio)) #记录下结果
-
-
-
-comparisonImageList.sort(key=lambda x:x[1],reverse=True) #按照匹配度排序
-count=len(comparisonImageList)
-column=2
-row=math.ceil(count/column)
-
-
-#绘图显⽰
-figure,ax=plt.subplots(row,column)
-for index,(image,ratio) in enumerate(comparisonImageList):
-    ax[int(index/column)][index%column].set_title('Similiarity %.2f%%' % ratio)
-    ax[int(index/column)][index%column].imshow(image)
-plt.show()
+     # 绘图显⽰
+    count=len(comparisonImageList)
+    column=2
+    row=math.ceil(count/column)
+    figure,ax=plt.subplots(row,column)
+    for index,(image,ratio) in enumerate(comparisonImageList):
+        ax[int(index/column)][index%column].set_title('Similiarity %.2f%%' % ratio)
+        ax[int(index/column)][index%column].imshow(image)
+    plt.show()
 
 
 

c99814f82dc8db8c96fabc2db77f73961697ef42
whyboy <1032131939@qq.com>
2022-05-14 23:50:02 +0800

    code refactor to make it use the args

--- ImageSearch.py
+++ ImageSearch.py
@@ -1,9 +1,9 @@
-
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 import os
 import math
+import sys
 
 class searcher:
     def __init__(self, samplePath, queryPath):
@@ -24,10 +24,10 @@ class searcher:
     def __loadAllImages(self):
         imageCollection=[]
         for parent,dirnames,filenames in os.walk(self.queryPath):
-            for p in filenames:
-                p=queryPath+p
-                image=cv2.imread(p,0)
-                imageCollection.append(image)
+            for imgName in filenames:
+                filePath=queryPath+imgName
+                image=cv2.imread(filePath,0)
+                imageCollection.append((image, imgName))
         return imageCollection
 
 
@@ -41,7 +41,7 @@ class searcher:
         #         p=queryPath+p
         #         queryImage=cv2.imread(p,0)
 
-        for queryImage in self.imageCollection:
+        for (queryImage, imageName) in self.imageCollection:
                 kp2, des2 = self.sift.detectAndCompute(queryImage, None) #提取⽐对图⽚的特征
                 matches=self.flann.knnMatch(des1,des2,k=2) #匹配特征点，为了删选匹配点，指定k为2，这样对样本图的每个特征点，返回两个匹配
                 (matchNum,matchesMask)=self.__getMatchNum(matches,0.9) #通过⽐率条件，计算出匹配程度
@@ -50,8 +50,9 @@ class searcher:
                         singlePointColor=(255,0,0),
                         matchesMask=matchesMask,
                         flags=0)
+                
                 comparisonImage=cv2.drawMatchesKnn(sampleImage,kp1,queryImage,kp2,matches,None,**drawParams)
-                comparisonImageList.append((comparisonImage,matchRatio)) #记录下结果
+                comparisonImageList.append((comparisonImage,matchRatio, imageName)) #记录下结果
 
         comparisonImageList.sort(key=lambda x:x[1],reverse=True) #按照匹配度排序
         return comparisonImageList
@@ -69,22 +70,28 @@ class searcher:
 
 
 if __name__ == "__main__":
-    path='C:/Users/whaiyan/Desktop/PictureSearchPicture/'
-    queryPath=path+'Pictures/' #图库路径
-    samplePath=path+'Sample/sample.PNG' #样本图⽚
+    # path='C:/Users/whaiyan/Desktop/PictureSearchPicture/'
+    # queryPath=path+'Pictures/' #图库路径
+    # samplePath=path+'Sample/sample.PNG' #样本图⽚
 
+    samplePath=sys.argv[1]
+    queryPath=sys.argv[2]
+    
     obj = searcher(samplePath, queryPath)
     comparisonImageList=obj.start()
+    topMatchImageName=comparisonImageList[0][2]
+    print(topMatchImageName)
 
-     # 绘图显⽰
-    count=len(comparisonImageList)
-    column=2
-    row=math.ceil(count/column)
-    figure,ax=plt.subplots(row,column)
-    for index,(image,ratio) in enumerate(comparisonImageList):
-        ax[int(index/column)][index%column].set_title('Similiarity %.2f%%' % ratio)
-        ax[int(index/column)][index%column].imshow(image)
-    plt.show()
+    #  # 绘图显⽰
+    # count=len(comparisonImageList)
+    # column=2
+    # row=math.ceil(count/column)
+    # figure,ax=plt.subplots(row,column)
+    # for index,(image,ratio, imageName) in enumerate(comparisonImageList):
+    #     print(imageName)
+    #     ax[int(index/column)][index%column].set_title('Similiarity %.2f%%' % ratio)
+    #     ax[int(index/column)][index%column].imshow(image)
+    # plt.show()
 
 
 
